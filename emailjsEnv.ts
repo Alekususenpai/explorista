64e7aef6b58a99642b217047f49d8b313022fa71
Alekususenpai <83718750+Alekususenpai@users.noreply.github.com>
2024-07-31 20:10:18 -0500

    Variable name fix

---
+++ emailjsEnv.ts
@@ -0,0 +1,7 @@
+const emailjsEnv = {
+    VITE_APP_EMAILJS_SERVICE_ID: 'service_hl4ixpi',
+    VITE_APP_EMAILJS_TEMPLATE_ID: 'template_4ar72r8',
+    VITE_APP_EMAILJS_PUBLIC_KEY: 'Ul8elbUClo_jPphgC'
+};
+
+export default emailjsEnv;
